fe42094dca35716af29715831dfd2bfdfd6f2e1a
Imo 'Vortex Acherontic' Hester <vortex@z-ray.de>
2024-01-18 14:51:56 +0100

    Improved create-pot script

--- create-pot.sh
+++ create-pot.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-/home/dvlv/.cargo/bin/xtr src/main.rs -o po/boxbuddy.pot
+XTR=$HOME/.cargo/bin/xtr
+
+function update_pod(){
+  echo "Generating new pod file..."
+  $HOME/.cargo/bin/xtr src/main.rs -o po/boxbuddy.pot
+  echo "Done. New pot file created, please translate."
+}
+
+function setup_dependencies(){
+  echo "Install xtr using cargo..."
+  cargo install xtr
+}
+
+[ -f $XTR ] && echo "Dependencies: OK" || setup_dependencies
+
+update_pod
