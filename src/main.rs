aca80f60cdfd3720fdbf7a446e9b8cd79343ba05
Dvlv <Dvlv@users.noreply.github.com>
2024-05-03 18:03:54 +0100

    bump version and gnome sdk

--- src/main.rs
+++ src/main.rs
@@ -858,7 +858,7 @@ fn show_about_popup(window: &ApplicationWindow) {
     let d = adw::AboutWindow::new();
     d.set_transient_for(Some(window));
     d.set_application_name("BoxBuddy");
-    d.set_version("2.2.2");
+    d.set_version("2.2.3");
     d.set_developer_name("Dvlv");
     d.set_license_type(gtk::License::MitX11);
     d.set_comments(
@@ -872,7 +872,7 @@ fn show_about_popup(window: &ApplicationWindow) {
     d.set_developers(&["Dvlv", "VortexAcherontic"]);
     d.set_application_icon("io.github.dvlv.boxbuddyrs");
     d.set_translator_credits(
-        "Vovkiv - RU and UK\nalbanobattistella - IT\nVortexAcherontic - DE\nLuiz-C-Lima - pt_BR",
+        "Vovkiv - RU and UK\nalbanobattistella - IT\nVortexAcherontic - DE\nLuiz-C-Lima - pt_BR\nPyrofanis - EL\nSebrice - ES\nScrambled777 - Hi\nMLSci - CN"
     );
     d.present();
 }
