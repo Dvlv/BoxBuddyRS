ea4382957ee63fca2e28be49cf1738d260552bae
Dvlv <Dvlv@users.noreply.github.com>
2024-09-09 14:30:22 +0100

    adjust zh translations

--- src/main.rs
+++ src/main.rs
@@ -869,7 +869,7 @@ fn show_about_popup(window: &ApplicationWindow) {
     let d = adw::AboutWindow::new();
     d.set_transient_for(Some(window));
     d.set_application_name("BoxBuddy");
-    d.set_version("2.2.11");
+    d.set_version("2.2.12");
     d.set_developer_name("Dvlv");
     d.set_license_type(gtk::License::MitX11);
     // TRANSLATORS: Description of the application
