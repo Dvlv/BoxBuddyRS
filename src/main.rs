1c879f74afd5f647d58e0dff813b0adf40c1583b
Dvlv <Dvlv@users.noreply.github.com>
2025-01-25 10:47:45 +0000

    bump version

--- src/main.rs
+++ src/main.rs
@@ -917,7 +917,7 @@ fn show_about_popup(window: &ApplicationWindow) {
     let d = adw::AboutWindow::new();
     d.set_transient_for(Some(window));
     d.set_application_name("BoxBuddy");
-    d.set_version("2.5.2");
+    d.set_version("2.5.3");
     d.set_developer_name("Dvlv");
     d.set_license_type(gtk::License::MitX11);
     // TRANSLATORS: Description of the application
